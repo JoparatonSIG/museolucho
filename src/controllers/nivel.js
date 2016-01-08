8626196bce6fd3db613a1ebeca4ae7f2a49c5154
luchobenitez <luchobenitez@gmail.com>
2016-01-08 04:40:09 -0300

    Se agrego el controlador de nivel

---
+++ src/controllers/nivel.js
@@ -0,0 +1,97 @@
+'use strict';
+
+// NIVEL CRUD
+
+var express = require('express');
+var router = express.Router();
+
+var Museo = require('../models/museo.js');
+
+// Rutas que terminan en /nivel
+
+// POST /nivel
+router.post('/nivel', function(req, res) {
+
+  // bodyParser debe hacer la magia
+	var categoria = req.body.categoria;
+
+	var nivel = Museo.Nivel.build({ categoria: categoria });
+
+	nivel.add(function(success){
+		res.json({ message: 'Nivel creado!' });
+	},
+	function(err) {
+		res.send(err);
+	});
+});
+
+// (trae todos los niveles)
+// GET /nivel
+router.get('/nivel', function(req, res) {
+	var nivel = Museo.Nivel.build();
+
+	nivel.retrieveAll(function(niveles) {
+		if (niveles) {
+		  res.json(niveles);
+		} else {
+		  res.send(401, 'No se encontraron Niveles');
+		}
+	}, function(error) {
+		res.send('Nivel no encontrado');
+	});
+});
+
+// Rutas que terminan en /nivel/:nivelId
+
+// PUT /nivel/:nivelId
+// Actualiza nivel
+router.put('/nivel/:nivelId', function(req, res) {
+	var nivel = Museo.Nivel.build();
+
+	nivel.categoria = req.body.categoria;
+
+	nivel.updateById(req.params.nivelId, function(success) {
+		console.log(success);
+		if (success) {
+			res.json({ message: 'Nivel actualizado!' });
+		} else {
+		  res.send(401, 'Nivel no encontrado');
+		}
+	}, function(error) {
+		res.send('Nivel no encontrado');
+	});
+});
+
+// GET /nivel/:nivelId
+// Toma un nivel por id
+router.get('/nivel/:nivelId', function(req, res) {
+	var nivel = Museo.Nivel.build();
+
+	nivel.retrieveById(req.params.nivelId, function(nivel) {
+		if (nivel) {
+		  res.json(nivel);
+		} else {
+		  res.send(401, 'Nivel no encontrado');
+		}
+	}, function(error) {
+		res.send('Nivel no encontrado');
+	});
+});
+
+// DELETE /nivel/nivelId
+// Borra el nivelId
+router.delete('/nivel/:nivelId', function(req, res) {
+	var nivel = Museo.Nivel.build();
+
+	nivel.removeById(req.params.nivelId, function(nivel) {
+		if (nivel) {
+		  res.json({ message: 'Nivel borrado!' });
+		} else {
+		  res.send(401, 'Nivel no encontrado');
+		}
+	}, function(error) {
+		res.send('Nivel no encontrado');
+	});
+});
+
+module.exports = router;
--- src/models/nivel.js
+++ src/models/nivel.js
@@ -6,14 +6,68 @@ module.exports = function (sequelize, DataTypes) {
         type: DataTypes.BIGINT(11),
         primaryKey: true,
         autoIncrement: true,
-        comment: 'nid, Primary Key, User ID'
+        comment: 'ID nivel'
       },
       categoria: {
         type: DataTypes.STRING(255),
         allowNull: false,
-        defaultValue: 'none',
-        comment: 'none, Categoria'
+        defaultValue: 'categoria',
+        comment: 'Categoria del Nivel',
+        validate: {
+          is: ["[a-z]",'i'],
+          notNull: true,
+          notEmpty: true
+        }
       }
+    },
+    {
+      instanceMethods: {
+        retrieveAll: function (onSuccess, onError) {
+          Nivel.findAll({})
+          .then(onSuccess).catch(onError);
+        },
+        retrieveById: function(nivelId, onSuccess, onError) {
+          Nivel.find({where: {id: nivelId}}, {raw: true})
+          .then(onSuccess).catch(onError);
+        },
+        retrieveByCategoria: function(nivelCategoria, onSuccess, onError) {
+          Nivel.find({where: {categoria: nivelCategoria}}, {raw: true})
+          .then(onSuccess).catch(onError);
+        },
+        add: function(onSuccess, onError) {
+          var categoria = this.categoria;
+
+          Nivel.build({ categoria: categoria })
+          .save().then(onSuccess).catch(onError);
+        },
+        updateById: function(nivelId, onSuccess, onError) {
+          var id = nivelId;
+
+          Nivel.update({ categoria: categoria},{where: {id: id} })
+          .then(onSuccess).catch(onError);
+        },
+        removeById: function(nivelId, onSuccess, onError) {
+          Nivel.destroy({where: {id: nivelId}})
+          .then(onSuccess).catch(onError);
+         }
+      },
+      timestamps: true,
+      paranoid:true,
+      createdAt: 'fechaCrea',
+      updatedAt: 'fechaModifica',
+      deletedAt: 'fechaBorra',
+      underscore: false,
+      freezeTableName:true,
+      tableName: 'Niveles',
+      comment: 'Niveles de acceso del Usuario al sistema',
+      indexes: [
+        {
+          name: 'idxCategoria',
+          method: 'BTREE',
+          unique: true,
+          fields: ['categoria']
+        }
+      ]
     }
   );
   return Nivel;
