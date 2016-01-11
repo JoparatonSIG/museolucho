37703a47009f5cd3e10e48e6f6134a4ce92c2cc0
Matias Insaurralde <matias@insaurral.de>
2016-01-11 02:38:59 -0300

    importar ejs layouts

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
 
 var accesorio = require('./controllers/accesorio');
 var analisis = require('./controllers/analisis');
@@ -24,6 +25,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(expressLayouts); // cargar el módulo de layouts
+app.set('layout', 'layout') // layout por defecto
+
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
