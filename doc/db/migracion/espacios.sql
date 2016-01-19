51d5de2575f9901d87cd2b4b80b1f3ceb3f4d9fa
techi <techisamudio@hotmail.com>
2016-01-19 18:08:11 -0300

    agregando sql de espacios

---
+++ doc/db/migracion/espacios.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+  museo.Espacios
+  (
+    id,
+    espacio,
+    codigoEspacio,
+    inmuebles,
+    codigoInmueble,
+    ubicacionInmueble,
+    creacion,
+    modifica
+  )
+  SELECT
+  IdEspacio,
+  Espacio,
+  CodigoEspacio,
+  Inmuebles,
+  CodigoInmueble,
+  UbicaciónInmueble,
+    now(),
+    now()
+FROM museoOriginal.espacios ori
+ORDER BY ori.IdEspacio ASC;
